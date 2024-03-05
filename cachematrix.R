622c67ec9e416cb89bbd423edc60fb17ac52426e
TriniAbiuso <161509000+TriniAbiuso@users.noreply.github.com>
2024-03-05 18:03:53 -0500

    Update cachematrix.R
    adding solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The function accepts a square matrix (assuming invertibility). It stores the matrix and initializes an inversed value set to null. It then declares 
+## several getters and setters function to get and set the value of the matrix as well as to get and set the inverse value. It returns these functions in a list.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat=matrix()){
+  inv <- NULL 
+  setMat <- function(val){
+    mat <<- val
+    inv <<- NULL
+  }
+  getMat <- function() mat
+  setInverse <- function(inversed) inv <<- inversed
+  getInverse <- function() inv
+  list(getMat=getMat, setMat=setMat, setInverse=setInverse, getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function gets the list created by the makeCacheMatrix function and can now access the matrix. It checks whether the inverse has already been calculated or not, and if so it
+## returns the cached value of the inverse. If the cached value has not been calculated yet it will compute the inverse using the solve() built-in function and will set the cache 
+## using the setInverse function.
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(mat, ...){
         ## Return a matrix that is the inverse of 'x'
+
+  cachedInversed <- mat$getInverse()
+  if(!is.null(cachedInversed)){
+    return(cachedInversed)
+  }
+  inversedMat <- solve(mat$getMat(), ...)
+  mat$setInverse(inversedMat)
+  inversedMat
+  
 }
